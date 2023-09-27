21629dd40307d697b99ad8b461f4dff02c98cc86
Gustavo Vono <guvono4@gmail.com>
2023-09-27 20:43:05 -0300

    feat: add new errors treatments for login route in backend

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -7,6 +7,8 @@ import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -16,19 +18,26 @@ public class UserService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public String userLogin(String username, String password) {
+    public Map<String, Object> userLogin(String username, String password) {
         Optional<UserEntity> userOptional = userRepository.findUserEntityByLogin(username);
+        Map<String, Object> response = new HashMap<>();
 
         if (userOptional.isPresent()) {
             UserEntity user = userOptional.get();
             if (password.equals(user.getPassword())) {
-                return "Login feito com sucesso! Role: " + user.getRole();
+                response.put("message", "Login realizado com sucesso.");
+                response.put("status", 200);
+                response.put("role", user.getRole());
             } else {
-                return "Login ou senha incorreto.";
+                response.put("message", "Login ou senha incorretos.");
+                response.put("status", 401);
             }
         } else {
-            return "Usuário não encontrado.";
+            response.put("message", "Usuário não encontrado.");
+            response.put("status", 404);
         }
+
+        return response;
     }
 
     public UserOrdersRequest getUser(Integer id) {
