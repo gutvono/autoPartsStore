91d0a10b12b37d5bea6d57315bc5fd7055665134
Gustavo Vono <guvono4@gmail.com>
2023-09-28 19:15:57 -0300

    feat: create home component page and styling

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-    // { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent }
 ];
 
