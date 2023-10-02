83cc544acce70e5a140346bf4835bdc3cb39a47e
Gustavo Vono <guvono4@gmail.com>
2023-10-02 13:30:02 -0300

    style: css updates

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-    public user:string = "Gustavo";
+    public user:string = "";
 
 }
