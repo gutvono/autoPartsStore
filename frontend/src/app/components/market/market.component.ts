327ae2fa65626217ecc0dccad22c6d74c2e294bf
Gustavo Vono <guvono4@gmail.com>
2023-10-02 17:41:44 -0300

    feat!: big changes in market page
    - fetch ok
    - now the products appear in page
    - filter structure created but not working properly yet
    - add all html structure and basics css

--- frontend/src/app/components/market/market.component.ts
+++ frontend/src/app/components/market/market.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MarketService } from './market.service';
+import { AutoPart } from './IAutoPart';
 
 @Component({
   selector: 'app-market',
@@ -6,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./market.component.scss']
 })
 export class MarketComponent {
+    public selectedCategory:string = '';
+    public allAutoParts: AutoPart[] = [];
+    public filteredAutoParts: AutoPart[] = [];
 
+    filterForm = new FormGroup({
+        name: new FormControl(''),
+        maxPrice: new FormControl(9999)
+    });
+
+    constructor(
+        private marketService: MarketService
+    ) {}
+
+    ngOnInit() {
+        this.marketService.getAllAutoParts()
+            .subscribe((parts: AutoPart[]) => {
+                this.allAutoParts = parts;
+                this.filteredAutoParts = parts;
+            });
+    };
+
+    get partName() {
+        return this.filterForm.get('name');
+    };
+
+    get maxPrice() {
+        return this.filterForm.get('maxPrice');
+    };
+
+    applyFilter(category: string | null): void {
+        if (category) {
+            this.selectedCategory = category;
+        }
+
+        console.log(this.partName?.value, this.maxPrice?.value, this.selectedCategory);
+        
+        this.filteredAutoParts = this.allAutoParts.filter((part: AutoPart) => {
+            
+        })
+
+        console.log(this.allAutoParts);
+        console.log(this.filteredAutoParts);
+    };
+
+    filterReset(): void {
+        this.partName?.setValue('');
+        this.maxPrice?.setValue(9999);
+        this.selectedCategory = '';
+    };
 }
