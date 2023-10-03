f791843394deaa5df4cc324f1c4cd153cfaba1aa
Gustavo Vono <guvono4@gmail.com>
2023-10-03 16:18:40 -0300

    fix: category filter working perfectly
    - all filters work together
    - when deleting a character the filter did not work, this has been fixed

--- frontend/src/app/components/market/market.component.ts
+++ frontend/src/app/components/market/market.component.ts
@@ -10,7 +10,9 @@ import { AutoPart } from './IAutoPart';
 })
 export class MarketComponent {
     public allAutoParts: AutoPart[] = [];
+    public autoPartsFilteredByCategory: AutoPart[] = [];
     public filteredAutoParts: AutoPart[] = [];
+    public isCategoryApplied: boolean = false;
 
     filterForm = new FormGroup({
         name: new FormControl(''),
@@ -40,15 +42,37 @@ export class MarketComponent {
     applyFilter(): void {
         const { name, maxPrice } = this.filterForm.value;
         
-        this.filteredAutoParts = this.allAutoParts.filter((part: AutoPart) => {
-            if (part.partName.toLowerCase().includes(name!.toLowerCase()) && part.price < maxPrice!) return true;
+        const searchWords = name!.toLowerCase().split(' ');
+
+        const arrayToFilter = this.isCategoryApplied ? this.autoPartsFilteredByCategory : this.allAutoParts;
+        
+        this.filteredAutoParts = arrayToFilter.filter((part: AutoPart) => {
+            const partNameLC = part.partName.toLowerCase();
+            const carNameLC = part.carName.toLowerCase();
+            if (
+                searchWords.every(word => partNameLC.includes(word) || carNameLC.includes(word))
+                && 
+                part.price < maxPrice!
+            ) return true;
+
             return false;
-        })
+        });
+    };
+
+    applyCategoryFilter(category: string): void {
+        this.marketService.getAutoPartsByCategoryFilter(category)
+            .subscribe((parts: AutoPart[]) => {
+                this.autoPartsFilteredByCategory = parts;
+                this.filteredAutoParts = parts;
+            });
+
+        this.isCategoryApplied = true;
     };
 
     filterReset(): void {
         this.partName?.setValue('');
         this.maxPrice?.setValue(9999);
+        this.isCategoryApplied = false;
         this.filteredAutoParts = this.allAutoParts;
     };
 }
